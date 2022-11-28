28fadfac639b0594d12f7c19ef03b51551be692b
GreatSage <michaelcull9715@gmail.com>
2022-11-28 21:07:08 +0300

    fc

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 
-import {createRoot} from 'react-dom/client'
+
+import ReactDOM from 'react-dom/client';
 import App from './App'
 import ContextProvider from './context/ContextProvider'
 
-
-const Root(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <ContextProvider>
               <App/>
   </ContextProvider>
   
   
-
 )
